3ff014ae231ff29fd6fc9826ea5d3fff284571f1
Bogdan <bogdan.plockiy@mail.ru>
2023-11-15 00:10:11 +0300

    вроде как сделал 1.1, надо разобраться с cli нормально

--- Konstr_PO_lab2/cppWinConsStack.cpp
+++ Konstr_PO_lab2/cppWinConsStack.cpp
@@ -1,34 +1,42 @@
-
+﻿// Инфа тут 
+// https://learn.microsoft.com/en-us/cpp/dotnet/how-to-create-clr-console-applications-cpp-cli?view=msvc-170
 #include "stdafx.h"
 using namespace System;
-using namespace std;
 
 ref class Stack {
 private:
     cli::array<String^>^ st;
     int top;
+
 public:
-    Stack() {
+    Stack()
+	{
         st = gcnew cli::array<String^>(100);
         top = 0;
     }
 
-    void push(String^ s) {
-        if (top < 100) {
+    void push(String^ s)
+	{
+        if (top < 100)
+		{
             st[top] = s;
             top++;
         }
-        else {
+        else
+		{
             Console::WriteLine("Stack is full");
         }
     }
 
-    String^ pop() {
-        if (top > 0) {
+    String^ pop()
+	{
+        if (top > 0)
+		{
             top--;
             return st[top];
         }
-        else {
+        else 
+		{
             Console::WriteLine("Stack is empty");
             return nullptr;
         }
@@ -43,12 +51,14 @@ int main(cli::array<System::String^>^ args)
     do {
         Console::WriteLine("What to do?");
         input = Console::ReadLine();
-        if (input == "push") {
+        if (input == "push")
+		{
             Console::WriteLine("Enter a string to push:");
             String^ s = Console::ReadLine();
             stack->push(s);
         }
-        else if (input == "pop") {
+        else if (input == "pop") 
+		{
             Console::WriteLine(stack->pop());
         }
     } while (input != "exit");
--- Konstr_PO_lab2/stdafx.h
+++ Konstr_PO_lab2/stdafx.h
@@ -1,9 +1,6 @@
-#ifndef STDAFX_H
+﻿#ifndef STDAFX_H
 #define STDAFX_H
 
-#include <iostream>
-#include <array>
 
-using namespace std;
 
 #endif // !STDAFX_H
