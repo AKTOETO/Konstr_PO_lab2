797b93c16897149f1d58d3f3e50b86af7e6deec6
Bogdan <bogdan.plockiy@mail.ru>
2023-11-15 21:27:34 +0300

    1.1 со стеком без класса сделано

--- Konstr_PO_lab2/cppWinConsStack.cpp
+++ Konstr_PO_lab2/cppWinConsStack.cpp
@@ -3,65 +3,115 @@
 #include "stdafx.h"
 using namespace System;
 
-ref class Stack {
-private:
-    cli::array<String^>^ st;
-    int top;
-
-public:
-    Stack()
-	{
-        st = gcnew cli::array<String^>(100);
-        top = 0;
-    }
-
-    void push(String^ s)
-	{
-        if (top < 100)
-		{
-            st[top] = s;
-            top++;
-        }
-        else
-		{
-            Console::WriteLine("Stack is full");
-        }
-    }
-
-    String^ pop()
-	{
-        if (top > 0)
-		{
-            top--;
-            return st[top];
-        }
-        else 
-		{
-            Console::WriteLine("Stack is empty");
-            return nullptr;
-        }
-    }
-};
-
-int main(cli::array<System::String^>^ args)
+// 1.1 стек строк (не класс)
+static int MaxSize = 100;
+static int top = 0;
+static void push(String^ c, array<String^>^ st)
 {
-    Stack^ stack = gcnew Stack();
-    String^ input;
+	// если максимальный размер стека не превышен
+	// то можем добавить элемент
+	if (top < MaxSize)
+		st[top++] = c;
 
-    do {
-        Console::WriteLine("What to do?");
-        input = Console::ReadLine();
-        if (input == "push")
-		{
-            Console::WriteLine("Enter a string to push:");
-            String^ s = Console::ReadLine();
-            stack->push(s);
-        }
-        else if (input == "pop") 
-		{
-            Console::WriteLine(stack->pop());
-        }
-    } while (input != "exit");
-
-    return 0;
+	// иначе не добавляем
+	else Console::WriteLine("Стек переполнен!");
 }
+static String^ pop(array<String^>^ st)
+{
+	// если стек не пуст
+	if (top > 0)
+		return st[--top];
+
+	// если же стек пуст
+	Console::WriteLine("Стек пуст!");
+	return "";
+}
+int main(array<String^>^ args)
+{
+	// создаем стек
+	array<String^>^ ST = gcnew array<String^>(MaxSize);
+
+	// строка для работы с консолью
+	String^ s;
+
+	// главный цикл
+	do
+	{
+		Console::WriteLine("Что сделать?");
+		s = Console::ReadLine();
+		
+		if (s == "заложить")
+		{
+			Console::WriteLine("Что?");
+			push(Console::ReadLine(), ST);
+		}
+
+		if (s == "достать")
+			Console::WriteLine(pop(ST));
+
+	} while (s != "");
+}
+
+
+//ref class Stack {
+//private:
+//    cli::array<String^>^ st;
+//    int top;
+//
+//public:
+//    Stack()
+//	{
+//        st = gcnew cli::array<String^>(100);
+//        top = 0;
+//    }
+//
+//    void push(String^ s)
+//	{
+//        if (top < 100)
+//		{
+//            st[top] = s;
+//            top++;
+//        }
+//        else
+//		{
+//            Console::WriteLine("Stack is full");
+//        }
+//    }
+//
+//    String^ pop()
+//	{
+//        if (top > 0)
+//		{
+//            top--;
+//            return st[top];
+//        }
+//        else 
+//		{
+//            Console::WriteLine("Stack is empty");
+//            return nullptr;
+//        }
+//    }
+//};
+//
+//int main(cli::array<System::String^>^ args)
+//{
+//    Stack^ stack = gcnew Stack();
+//    String^ input;
+//
+//    do {
+//        Console::WriteLine("What to do?");
+//        input = Console::ReadLine();
+//        if (input == "push")
+//		{
+//            Console::WriteLine("Enter a string to push:");
+//            String^ s = Console::ReadLine();
+//            stack->push(s);
+//        }
+//        else if (input == "pop") 
+//		{
+//            Console::WriteLine(stack->pop());
+//        }
+//    } while (input != "exit");
+//
+//    return 0;
+//}
